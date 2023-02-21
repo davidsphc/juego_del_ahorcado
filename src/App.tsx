9007b874d191e3ac19c9b92bfce33dab6144f045
davidsphc <hernandezdavidsph@gmail.com>
2023-02-20 19:21:24 -0600

    continuacion de proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-
+import { letters } from './helpers/letters';
 import './App.css';
+import { HangImage } from './components/Hanglmage';
+import { useState } from 'react';
+
 function App() {
-return(
-  <div className="App">
-    <h1>Hola Mundo</h1> 
+
+ const [ attemps, setAttemps ] = useState(0)
+
+const checkLetter = (letter : string) => {
+  console.log(letter)
+  setAttemps(Math.min (attemps + 1, 9 ) );
+}
+
+
+    return(
+      <div className="App">
+          {/* Imagenes del juego */}
+          <HangImage imageNumber={attemps} />
+
+          {/* Palabra oculta */}
+          <h3>_ _ _ _ _ _ _ _ _ _ _ _</h3>
+
+          {/* Contador de intentos */}
+          <h3>Intentos:{attemps} </h3>
+          {
+            letters.map((letter)=>(
+              <button 
+                onClick={ () => checkLetter(letter)}
+                key ={ letter} > 
+                { letter }
+              </button>
+            ))                                    
+          }         
   </div>
-  )
+  );
 
 };
 
